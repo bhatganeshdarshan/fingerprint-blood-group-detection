9ab1a3b7fe1b9535a5e33450ed72d10d899f1539
Ganeshdarshan <bhatganeshdarshan10@gmail.com>
2024-09-24 20:01:07 +0530

    changed pages

---
+++ src/navigation.ts
@@ -0,0 +1,134 @@
+import { getPermalink, getBlogPermalink, getAsset } from './utils/permalinks';
+
+export const headerData = {
+  links: [
+    {
+      text: 'Services',
+      links: [
+        {
+          text: 'Request Blood',
+          href: getPermalink('/homes/saas'),
+        },
+        {
+          text: 'Donate Blood',
+          href: getPermalink('/homes/startup'),
+        },
+        {
+          text: ' Finger print Details',
+          href: getPermalink('/homes/mobile-app'),
+        },
+  
+      ],
+    },
+    {
+      text: 'Information',
+      links: [
+
+        {
+          text: 'Services',
+          href: getPermalink('/services'),
+        },
+        {
+          text: 'Pricing',
+          href: getPermalink('/pricing'),
+        },
+        {
+          text: 'About us',
+          href: getPermalink('/about'),
+        },
+        {
+          text: 'Contact',
+          href: getPermalink('/contact'),
+        },
+        {
+          text: 'Terms',
+          href: getPermalink('/terms'),
+        },
+        {
+          text: 'Privacy policy',
+          href: getPermalink('/privacy'),
+        },
+      ],
+    },
+    {
+      text: 'Support',
+      links: [
+        {
+          text: 'Customer Care',
+          href: getPermalink('/landing/lead-generation'),
+        },
+ 
+      
+      ],
+    },
+ 
+    {
+      text: 'Terms & Conditions Apply',
+      href: '#',
+    },
+  ],
+  
+};
+
+export const footerData = {
+  links: [
+    {
+      title: 'Product',
+      links: [
+        { text: 'Features', href: '#' },
+        { text: 'Security', href: '#' },
+        { text: 'Team', href: '#' },
+        { text: 'Enterprise', href: '#' },
+        { text: 'Customer stories', href: '#' },
+        { text: 'Pricing', href: '#' },
+        { text: 'Resources', href: '#' },
+      ],
+    },
+    {
+      title: 'Platform',
+      links: [
+        { text: 'Developer API', href: '#' },
+        { text: 'Partners', href: '#' },
+        { text: 'Blood', href: '#' },
+        { text: 'Resources', href: '#' },
+        { text: 'Detection', href: '#' },
+      ],
+    },
+    {
+      title: 'Support',
+      links: [
+        { text: 'Docs', href: '#' },
+        { text: 'Community Forum', href: '#' },
+        { text: 'Professional Services', href: '#' },
+        { text: 'Skills', href: '#' },
+        { text: 'Status', href: '#' },
+      ],
+    },
+    {
+      title: 'Company',
+      links: [
+        { text: 'About', href: '#' },
+        { text: 'Blog', href: '#' },
+        { text: 'Careers', href: '#' },
+        { text: 'Press', href: '#' },
+        { text: 'Inclusion', href: '#' },
+        { text: 'Social Impact', href: '#' },
+        { text: 'Shop', href: '#' },
+      ],
+    },
+  ],
+  secondaryLinks: [
+    { text: 'Terms', href: getPermalink('/terms') },
+    { text: 'Privacy Policy', href: getPermalink('/privacy') },
+  ],
+  socialLinks: [
+    { ariaLabel: 'X', icon: 'tabler:brand-x', href: '#' },
+    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
+    { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
+    
+    { ariaLabel: 'Github', icon: 'tabler:brand-github',  },
+  ],
+  footNote: `
+ All rights reserved.
+  `,
+};
